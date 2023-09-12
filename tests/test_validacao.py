21e545a040bb82660c9f8ba4560d81955ae609e1
Tiago Sanches da Silva <tiago.eem@gmail.com>
2023-09-12 08:59:31 -0300

    em aula

--- tests/test_validacao.py
+++ tests/test_validacao.py
@@ -1,7 +1,26 @@
 import pytest
 from ..validacao import valida_cpf, valida_idade
 
-@pytest.mark.entrada_de_dados
+@pytest.mark.cpf
 def test_valida_cpf_valido():
     assert valida_cpf("33652827610") == True
 
+@pytest.mark.cpf
+def test_valida_cpf_com_mais_de_11_digitos():
+    assert valida_cpf("336528276100") == False
+
+@pytest.mark.cpf
+def test_valida_cpf_com_menos_de_11_digitos():
+    assert valida_cpf("336528") == False
+
+@pytest.mark.cpf
+def test_valida_cpf_com_somente_numeros():
+    assert valida_cpf("336528A7610") == False
+
+@pytest.mark.cpf
+def test_valida_cpf_aceita_somente_strings():
+    assert valida_cpf(33652827610) == False
+
+@pytest.mark.idade
+def test_valida_idade_positiva():
+    assert valida_idade(5) == True
--- validacao.py
+++ validacao.py
@@ -1,5 +1,17 @@
-def valida_cpf(cpf: str) -> bool:
-    pass
+def valida_cpf(cpf):
+    
+    if not isinstance(cpf, str):
+        return False
+
+    if not cpf.isdigit():
+        return False
+
+    if len(cpf) != 11:
+        return False
+
+    return True
+
 
 def valida_idade(idade: int) -> bool:
-    pass
+    
+    return False
